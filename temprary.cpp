c6c81c933daeea99dafb9c1fba5e780f4363efaf
Ritik kumar <thakurritik38921@gmail.com>
2022-01-08 15:34:21 +0530

    longest increasing subsequence question solution

--- temprary.cpp
+++ temprary.cpp
@@ -2,35 +2,72 @@
 using namespace std;
 
 
+// finding the longest increasing sequences....
 
-// longest common sequences....
-
-int lcs(int* arr,int n1,int *arr1,int n2){
-	if(n1 == 0 || n2 == 0){
-		return 0;
+int longestIncreasingSubsequences(int *arr,int s,int n,int prevIndex,int currLen){
+	if(s == n){
+		return currLen;
 	}
 
-	if(arr[n1-1] == arr1[n2-1]){
-		return 1 + lcs(arr,n1-1,arr1,n2-1);
-	}else{
-		int a = lcs(arr,n1,arr1,n2-1);
-		int b = lcs(arr,n1-1,arr1,n2);
-		return (a > b)?a:b;
+	if(prevIndex == -1){
+		int res1 = longestIncreasingSubsequences(arr,s+1,n,s,currLen+1);
+		int res2 = longestIncreasingSubsequences(arr,s+1,n,prevIndex,currLen);
+		if(res1 > res2){
+			return res1;
+		}
+		return res2;
+	}
+	else{
+		int res1,res2;
+		res1 = -1;
+		res2 = 0;
+		if(arr[s] > arr[prevIndex]){
+		 res1 = longestIncreasingSubsequences(arr,s+1,n,s,currLen+1);
+		}
+		 res2 = longestIncreasingSubsequences(arr,s+1,n,prevIndex,currLen);
+		if(res1 != -1){
+			if(res1 > res2){
+			return res1;
+		}
+		 	return res2;
+		}else{
+			return res2;
+		}
 	}
 }
 
+int lcs(int *arr,int n){
+	int *dp = new int[n];
+	dp[0] = 1;
+	for(int i = 1 ; i < n ; i++){
+		int max = INT_MIN;
+		for(int j = i-1; j >= 0 ; j--){
+			if(arr[i] > arr[j]){
+				max = (max < dp[j])?dp[j]:max;
+			}
+		}
+		dp[i] = max + 1;
+	}
+
+	for(int i = 0;i<n;i++){
+		cout << dp[i] << " ";
+	}
+
+	return dp[n - 1];
+}
 
 int main(){
-	int arr[] = {3,7,10,40,50,80};
-	int arr1[] = {50,3,10,7,40,80};
-	int n = sizeof(arr)/sizeof(int);
-	int ans = lcs(arr,n,arr1,n);
-	cout << " longest common subsequnces are : "<< ans << endl;
-	return 0;
+	int n;
+	cin >> n;
+	int *arr = new int[n];
+	for(int i = 0;i<n;i++){
+		cin >> arr[i];
+	}
+
+	int ans = lcs(arr,n);
+	cout << " \nlongest increasing SubSequence : "<< ans << endl;
+ 	return 0;
 }
-
-
-
 
 
 
